3d26d31063c68cfc06a7837248f5014cd0b59a12
hi991016 <miureus@gmail.com>
2025-02-23 10:44:20 +0700

    animation loading step 2 done :check:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,33 @@
 "use strict";
 
 const init = () => {
-  gsap.registerPlugin(ScrollTrigger);
+  // console.clear();
+  // gsap config
   ScrollTrigger.clearScrollMemory("manual");
+  gsap.registerPlugin(ScrollTrigger);
+  ScrollTrigger.refresh();
+  if (window.innerWidth < 1024) {
+    ScrollTrigger.config({
+      // a comma-delimited list of events that trigger a refresh.
+      // default: "visibilitychange,DOMContentLoaded,load,resize"
+      // so remove the "resize" one:
+      autoRefreshEvents: "visibilitychange,DOMContentLoaded,load",
+    });
+  }
   // app height
   appHeight();
   // init loader
   initLoader();
-  // scroll trigger logo shrink
-  let logo = document.querySelector(".jslogo"),
-    header = document.querySelector(".c-header_center"),
-    state = Flip.getState(logo); // record the state
-  header.insertBefore(logo, header.firstChild); // put the logo into the header (re-parent it)
-
-  let move = Flip.to(state, { duration: 1, ease: "power1.inOut" });
-  move.progress(1);
-  ScrollTrigger.create({
-    start: 100,
-    trigger: "#jsLogoShrink",
-    start: "top+=50 top",
-    end: "top top",
-    onEnter: () => {
-      move.reverse();
-      gsap.to("[data-header-logo], [data-scrolldown]", {
-        opacity: 0,
-        duration: 0.5,
-      });
-    },
-    onEnterBack: () => {
-      move.play();
-      gsap.to("[data-header-logo], [data-scrolldown]", {
-        opacity: 1,
-        duration: 0.5,
-        delay: 0.5,
-      });
-    },
-    markers: false,
-  });
+  // logo shrink
+  scrollLogoShrink();
 };
-
-// ===== app height =====
-const appHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty(
-    "--app-height",
-    `${document.documentElement.clientHeight}px`
-  );
-};
-window.addEventListener("resize", appHeight);
 
 // ===== lenis =====
 const lenis = new Lenis({
   lerp: 0.05,
   smoothWheel: true,
 });
-lenis.on("scroll", (e) => {});
+lenis.on("scroll", ScrollTrigger.update);
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
@@ -74,16 +47,16 @@ const initLoader = () => {
     .to("[data-loading-logo]", {
       opacity: 1,
       duration: 1,
-      delay: 0.5,
+      delay: 1,
       ease: Power4.easeInOut,
     })
     .to("[data-loading-overlay]", {
       top: 0,
-      duration: 1.5,
+      duration: 1.8,
       ease: Power4.easeOut,
     })
     .to(
-      "[data-heading] .jslogo",
+      "[data-logo-shrink]",
       {
         opacity: 1,
         duration: 1,
@@ -97,8 +70,64 @@ const initLoader = () => {
       ease: Power4.easeInOut,
     })
     .to("[data-loading]", {
-      top: "-100%",
+      zIndex: "-100",
     });
 };
 
+// ===== app height =====
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty(
+    "--app-height",
+    `${document.documentElement.clientHeight}px`
+  );
+};
+window.addEventListener("resize", appHeight);
+
+// ===== logo shirnk =====
+const scrollLogoShrink = () => {
+  // ==== create
+  let mmg = gsap.matchMedia(),
+    breakPoint = 1024;
+
+  mmg.add(
+    {
+      isDesktop: `(min-width: ${breakPoint}px)`,
+      isMobile: `(max-width: ${breakPoint - 1}px)`,
+    },
+    (context) => {
+      let { isMobile } = context.conditions;
+
+      ScrollTrigger.create({
+        animation: gsap.from("[data-logo-shrink]", {
+          height: "100%",
+          width: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+        }),
+        start: 100,
+        trigger: "[data-offset-top]",
+        start: "top+=50 top",
+        end: "top top",
+        toggleActions: "play none reverse none",
+        markers: false,
+        onEnter: () => {
+          gsap.to("[data-header-logo], [data-scrolldown]", {
+            opacity: 0,
+            duration: 0.5,
+          });
+        },
+        onEnterBack: () => {
+          gsap.to("[data-header-logo], [data-scrolldown]", {
+            opacity: 1,
+            duration: 0.5,
+            delay: 0.5,
+          });
+        },
+      });
+    }
+  );
+};
+
+// DOMContentLoaded
 window.addEventListener("DOMContentLoaded", init);
